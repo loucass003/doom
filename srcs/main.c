796b2b93c6a09220a1db2518bd692f33a686ee9c
loucass003 <loucass003@gmail.con>
2019-04-12 17:12:29 +0200

    meh ca marche po

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 14:54:33 by llelievr          #+#    #+#             */
-/*   Updated: 2019/04/12 15:18:24 by llelievr         ###   ########.fr       */
+/*   Updated: 2019/04/12 17:02:57 by llelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,12 +44,18 @@ typedef struct		s_node
 }					t_node;
 
 t_line lines[] = {
-	{ { 0, 150 }, { 0, 450 } },
+	/*{ { 0, 150 }, { 0, 450 } },
 	{ { 0, 450 }, { 800, 450 } },
 	{ { 800, 450 }, { 800, 300 } },
 	{ { 800, 300 }, { 500, 300 } },
 	{ { 500, 300 }, { 500, 75 } },
-	{ { 500, 75 }, { 0, 150 } }
+	{ { 500, 75 }, { 0, 150 } }*/
+	{ { 0, 150 }, { 500, 75 } },
+	{ { 500, 75 }, { 500, 300 } },
+	{ {500, 300}, { 800, 300 }},
+	{ {800, 300}, { 800, 450 }},
+	{ {800, 450}, {0, 450 }},
+	{ {0, 450}, {0, 150}}
 };
 
 t_bool	intersect(t_line *line, t_line *seg, t_vec2 *intersect)
@@ -78,14 +84,14 @@ t_bool	intersect(t_line *line, t_line *seg, t_vec2 *intersect)
 	return (TRUE);
 }
 
-t_bool	append_list(t_line_list **list, t_line *line)
+t_bool	append_list(t_line_list **list, t_line line)
 {
 	t_line_list	*n;
 	t_line_list	*elem;
 
 	if (!(elem = (t_line_list *)malloc(sizeof(t_line_list))))
 		return (FALSE);
-	elem->line = *line;
+	elem->line = line;
 	elem->next = NULL;
 	if (!*list)
 		*list = elem;
@@ -97,24 +103,6 @@ t_bool	append_list(t_line_list **list, t_line *line)
 		n->next = elem;
 	}
 	return (TRUE);
-}
-
-void	get_line_sides(t_line *line)
-{
-	t_vec2 i1 = {
-		line->a.x + (line->b.y - line->a.y),
-		line->a.y + -(line->b.x - line->a.x)
-	};
-	float d = (i1.x - line->b.x) * (line->b.y - line->a.y)
-	- (i1.y - line->a.y) * (line->b.x - line->a.x);
-
-	t_vec2 i2 = {
-		line->a.x + -(line->b.y - line->a.y),
-		line->a.y + (line->b.x - line->a.x)
-	};
-	float d2 = (i2.x - line->b.x) * (line->b.y - line->a.y)
-	- (i2.y - line->a.y) * (line->b.x - line->a.x);
-	printf("(%f, %f) (%f, %f) (%f, %f)\n", i1.x, i1.y, i2.x, i2.y, d, d2);
 }
 
 /*
@@ -131,30 +119,20 @@ d va aussi servir a savoir de quel cote sont les segements des autres sections
 */
 
 
-t_side	get_side(t_line *partition, t_line *seg)
+t_side	get_side(t_line *partition, t_line seg)
 {
 	const float dot = ft_vec2_cross(
 		ft_vec2_norm((t_vec2) { partition->b.x - partition->a.x, partition->b.y - partition->a.y }),
-		ft_vec2_norm((t_vec2) { seg->b.x - seg->a.x, seg->b.y - seg->a.y }));
+		ft_vec2_norm((t_vec2) { seg.b.x - seg.a.x, seg.b.y - seg.a.y }));
 	if (dot == 0)
 		return (S_COLINEAR);
 	return (dot < 0 ? S_BACK : S_FRONT);
 }
 
-void	append_line(t_line *p, t_line *line, t_line_list *front, t_line_list *back, int *front_count, int *back_count)
+void	append_line(t_line *p, t_line line, t_line_list **front, t_line_list **back, int *front_count, int *back_count)
 {
-	t_side side = get_side(p, line);
-	if (side == S_FRONT)
-	{
-		append_list(&front, line);
-		(*front_count)++;
+
 	}
-	else if (side == S_BACK)
-	{
-		append_list(&back, line);
-		(*back_count)++;
-	}
-}
 
 t_node	*build_node(t_line_list *lines, int lines_count)
 {
@@ -164,10 +142,12 @@ t_node	*build_node(t_line_list *lines, int lines_count)
 	int			back_count	= 0;
 	t_node		*node;
 
-	if (lines_count == 0 || !lines || !lines->next || !(node = (t_node *)malloc(sizeof(t_node))))
+	if (lines_count == 0 || !(node = (t_node *)malloc(sizeof(t_node))))
 		return (NULL);
 	node->segments = lines;
-	node->partition = lines->next->line;
+	node->partition = lines->line;
+	if (!lines->next)
+		return (node);
 	lines = lines->next;
 	while (lines)
 	{
@@ -176,23 +156,42 @@ t_node	*build_node(t_line_list *lines, int lines_count)
 		{
 			t_line a = { lines->line.a, it };
 			t_line b = { it, lines->line.b };
-			append_line(&node->partition, &a, front, back, &front_count, &back_count);
-			append_line(&node->partition, &b, front, back, &front_count, &back_count);
-		}
-		else
-		{
-			append_line(&node->partition, &lines->line, front, back, &front_count, &back_count);
-			/*t_side side = get_side(&node->partition, &lines->line);
+			t_side side = get_side(&node->partition, a);
 			if (side == S_FRONT)
 			{
-				append_list(&front, &lines->line);
+				append_list(&front, a);
 				front_count++;
 			}
 			else if (side == S_BACK)
 			{
-				append_list(&back, &lines->line);
+				append_list(&back, a);
 				back_count++;
-			}*/
+			}
+			side = get_side(&node->partition, b);
+			if (side == S_FRONT)
+			{
+				append_list(&front, b);
+				front_count++;
+			}
+			else if (side == S_BACK)
+			{
+				append_list(&back, b);
+				back_count++;
+			}
+		}
+		else
+		{
+			t_side side = get_side(&node->partition, lines->line);
+			if (side == S_FRONT)
+			{
+				append_list(&front, lines->line);
+				front_count++;
+			}
+			else if (side == S_BACK)
+			{
+				append_list(&back, lines->line);
+				back_count++;
+			}
 		}
 		//printf("(%d) (%f, %f)\n", side, it.x, it.y);
 		lines = lines->next;
@@ -200,7 +199,7 @@ t_node	*build_node(t_line_list *lines, int lines_count)
  
 	printf("build front node with %d segments\n", front_count);
 	node->front = build_node(front, front_count);
-	printf("build back node with %d segments\n", front_count);
+	printf("build back node with %d segments\n", back_count);
 	node->back = build_node(back, back_count);
 	return (node);
 }
@@ -220,11 +219,9 @@ int		main(void)
 	printf("(%f, %f) (%f, %f)\n", min.x, min.y, max.x, max.y);
 	t_line_list *lst = NULL;
 	for (int i = 0; i < count; i++)
-		append_list(&lst, &lines[i]);
+		append_list(&lst, lines[i]);
 	t_node *n = NULL;
-
 	n = build_node(lst, count);
-
 	if (!n)
 		return (-1);
 	return (0);
