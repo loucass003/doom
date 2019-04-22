074476f06027dc7c25fef3eddd927a60154084f3
loucass003 <loucass003@gmail.com>
2019-04-23 01:17:25 +0200

    Render walls YAY !

--- srcs/events.c
+++ srcs/events.c
@@ -6,7 +6,7 @@
 /*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 22:14:55 by llelievr          #+#    #+#             */
-/*   Updated: 2019/04/18 19:35:50 by llelievr         ###   ########.fr       */
+/*   Updated: 2019/04/22 23:04:08 by llelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,27 @@ static void	events_window(t_doom *doom, SDL_Event *event)
 
 void	hook_events(t_doom *doom)
 {
-	const Uint8		*state = SDL_GetKeyboardState(NULL);
+
+	const double	ms = doom->stats.delta * 300.;
+	const Uint8		*s = SDL_GetKeyboardState(NULL);
 	SDL_Event		event;
 
-	if (state[SDL_SCANCODE_ESCAPE])
+	if (s[SDL_SCANCODE_ESCAPE])
 		doom->running = FALSE;
 	t_vec2 dir = {0, 0};
-	if (state[SDL_SCANCODE_W] || state[SDL_SCANCODE_S])
-		dir.y += (state[SDL_SCANCODE_W] ? -1 : 1);
-	if (state[SDL_SCANCODE_A] || state[SDL_SCANCODE_D])
-		dir.x += (state[SDL_SCANCODE_D] ? -1 : 1);
-	if (state[SDL_SCANCODE_J] || state[SDL_SCANCODE_L])
-		doom->player.rotation += 0.01 * (state[SDL_SCANCODE_J] ? 1 : -1);
-	doom->player.pos = ft_vec2_add(doom->player.pos, ft_mat2_mulv(ft_mat2_rotation(doom->player.rotation + M_PI_2), dir));
+	if (s[SDL_SCANCODE_W] || s[SDL_SCANCODE_S])
+	{
+		dir.x += sinf(doom->player.rotation) * (s[SDL_SCANCODE_W] ? 1 : -1) * ms;
+		dir.y += cosf(doom->player.rotation) * (s[SDL_SCANCODE_W] ? 1 : -1) * ms;
+	}
+	if (s[SDL_SCANCODE_A] || s[SDL_SCANCODE_D])
+	{
+		dir.x += -cosf(doom->player.rotation) * (s[SDL_SCANCODE_D] ? 1 : -1) * ms;
+		dir.y += sinf(doom->player.rotation) * (s[SDL_SCANCODE_D] ? 1 : -1) * ms;
+	}
+	if (s[SDL_SCANCODE_J] || s[SDL_SCANCODE_L])
+		doom->player.rotation += 0.003 * (s[SDL_SCANCODE_J] ? 1 : -1) * ms;
+	doom->player.pos = ft_vec2_add(doom->player.pos, dir);
 	while (SDL_PollEvent(&event))
 		events_window(doom, &event);
 	SDL_PumpEvents();
--- srcs/render.c
+++ srcs/render.c
@@ -6,21 +6,12 @@
 /*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 16:49:48 by llelievr          #+#    #+#             */
-/*   Updated: 2019/04/22 18:59:31 by llelievr         ###   ########.fr       */
+/*   Updated: 2019/04/23 00:56:09 by llelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom.h"
-
-t_vec2	it(t_vec2 a, t_vec2 b, t_vec2 c, t_vec2 d)
-{
-	float x = ft_vec2_cross(a, b);
-	float y = ft_vec2_cross(c, d);
-	float det = ft_vec2_cross(ft_vec2_sub(a, b), ft_vec2_sub(c, d));
-	x = ft_vec2_cross((t_vec2){ x, a.x - b.x}, (t_vec2){y, c.x - d.x}) / det;
-	y = ft_vec2_cross((t_vec2){ x, a.y - b.y}, (t_vec2){y, c.y - d.y}) / det;
-	return (t_vec2){ x, y };
-}
+#include <assert.h>
 
 t_node	*player_node(t_doom *doom)
 {
@@ -39,7 +30,7 @@ t_node	*player_node(t_doom *doom)
 }
 
 #define H_FOV (0.73f * S_HEIGHT)  // Affects the horizontal field of vision
-#define V_FOV (0.2f * S_HEIGHT)    // Affects the vertical field of vision
+#define V_FOV (0.4f * S_HEIGHT)    // Affects the vertical field of vision
 
 void	draw_wall(t_doom *doom, t_line *line)
 {
@@ -52,49 +43,74 @@ void	draw_wall(t_doom *doom, t_line *line)
 	t_vec2 ta = ft_mat2_mulv(m, a);
 	t_vec2 tb = ft_mat2_mulv(m, b);
 
-	float heigthA = H_FOV / ta.y;
-
-	float heigthB = H_FOV / tb.y;
-
-	printf("%f %f %f %f\n", ta.x, tb.x, heigthA, heigthB);
-	if (ta.y <= 0 || tb.y <= 0)
+	if (ta.y <= 0 && tb.y <= 0)
 		return ;
 	if (ta.y <= 0 || tb.y <= 0)
 	{
-		t_vec2 i1 = it(ta, tb, (t_vec2){-0.00001, 0.0001}, (t_vec2){-20, 5});
-		t_vec2 i2 = it(ta, tb, (t_vec2){0.00001, 0.0001}, (t_vec2){20, 5});
-		if (ta.y <= 0.00001)
+		t_vec2 i1;
+		intersect(&(t_line){ta, tb}, &(t_line){{-0.01, 0.01}, (t_vec2){-20, 5}}, &i1);
+		t_vec2 i2;
+		intersect(&(t_line){ta, tb}, &(t_line){{0.01, 0.01}, (t_vec2){20, 5}}, &i2);
+		if (ta.y < 0.01)
 			ta = i1.y > 0 ? i1 : i2;
-		if (tb.y <= 0.00001)
+		if (tb.y < 0.01)
 			tb = i1.y > 0 ? i1 : i2;
 	}
+	float heigthA = H_FOV / ta.y * 100;
+	float heigthB = H_FOV / tb.y * 100;
 	int x1 = S_WIDTH_2 - ta.x * (V_FOV / ta.y);
 	int x2 = S_WIDTH_2 - tb.x * (V_FOV / tb.y);
-	draw_line(&doom->screen, (t_pixel){x1, S_HEIGHT_2 + (heigthA * 100) / 2, 0xFFFFFF}, (t_pixel){x2, S_HEIGHT_2 + (heigthB * 100) / 2});
-	draw_line(&doom->screen, (t_pixel){x1, S_HEIGHT_2 - (heigthA * 100) / 2, 0xFFFFFF}, (t_pixel){x2, S_HEIGHT_2 - (heigthB * 100) / 2});
-	draw_line(&doom->screen, (t_pixel){x1, S_HEIGHT_2 - (heigthA * 100) / 2, 0xFF0000}, (t_pixel){x1, S_HEIGHT_2 + (heigthA * 100) / 2});
-	draw_line(&doom->screen, (t_pixel){x2, S_HEIGHT_2 - (heigthB * 100)/ 2, 0x00FF00}, (t_pixel){x2, S_HEIGHT_2 + (heigthB * 100) / 2});
+	int color = 0xFFFFFF / fmax(1, ft_abs(line->a.x + line->b.x + 1));
+	for (int x = x1; x < x2; x++)
+	{
+		//printf("%d\n", (x2 - x1));
+		if (x2 - x1 == 0)
+			break;
+		//assert((heigthB - heigthA) < 0.001);
+		float h = heigthA + (x - x1) * ((heigthB - heigthA) / (x2 - x1));
+		draw_line(&doom->screen, (t_pixel){x, S_HEIGHT_2 - (h) / 2, color}, (t_pixel){x, S_HEIGHT_2 + (h) / 2});
+	}
+/*	draw_line(&doom->screen, (t_pixel){x1, S_HEIGHT_2 + (heigthA) / 2, 0xFFFFFF}, (t_pixel){x2, S_HEIGHT_2 + (heigthB) / 2});
+	draw_line(&doom->screen, (t_pixel){x1, S_HEIGHT_2 - (heigthA) / 2, 0xFFFFFF}, (t_pixel){x2, S_HEIGHT_2 - (heigthB) / 2});
+	draw_line(&doom->screen, (t_pixel){x2, S_HEIGHT_2 - (heigthB) / 2, 0x00FF00}, (t_pixel){x2, S_HEIGHT_2 + (heigthB) / 2});
+	draw_line(&doom->screen, (t_pixel){x1, S_HEIGHT_2 - (heigthA) / 2, 0xFF0000}, (t_pixel){x1, S_HEIGHT_2 + (heigthA) / 2});*/
+}
+
+void visitNode(t_doom *doom, t_node *node)
+{
+	t_line_list *lst = node->segments;
+	
+	while (lst)
+	{
+		draw_wall(doom, &lst->line);
+		lst = lst->next;
+	}
+}
+
+void traverseDrawOrder(t_doom *doom, t_node *node) 
+{
+	t_side side;
+	
+	if (node) 
+	{
+		if (node->type == N_LEAF)
+			visitNode(doom, node);
+		else if ((side = get_side_thin(&node->partition, doom->player.pos)) != S_BACK) 
+		{
+			traverseDrawOrder(doom, node->front);
+			visitNode(doom, node);
+			traverseDrawOrder(doom, node->back);
+		}
+		else 
+		{
+			traverseDrawOrder(doom, node->back);
+			visitNode(doom, node);
+			traverseDrawOrder(doom, node->front);
+		}
+	}
 }
 
 void	render(t_doom *doom)
 {
-	t_node *n = player_node(doom);
-	while(n)
-	{
-		
-		t_side side = get_side_thin(&n->partition, doom->player.pos);
-		if (side == S_FRONT)
-		{
-		//	draw_line(&doom->screen, (t_pixel){n->partition.a.x, n->partition.a.y, 0xFFFFFF}, (t_pixel){n->partition.b.x, n->partition.b.y});
-			draw_wall(doom, &n->partition);
-		}
-		n = n->parent;
-	}
-	doom->screen.pixels[(doom->screen.height / 2 * doom->screen.width) + doom->screen.width / 2] = 0xFF00;
-	draw_line(&doom->screen, (t_pixel){ doom->screen.width / 2, doom->screen.height / 2, 0xFFFFFF} , (t_pixel){doom->screen.width / 2 + (int)(5 * cosf(0)), doom->screen.height / 2 + (int)(5 * sinf(0))});
-
-//	draw_line(&doom->screen, (t_pixel){ doom->player.pos.x, doom->player.pos.y, 0xFFFFFF} , (t_pixel){(int)doom->player.pos.x + (int)(5 * cosf(doom->player.rotation)), (int)doom->player.pos.y + (int)(5 * sinf(doom->player.rotation))});
-//	doom->screen.pixels[((int)doom->player.pos.y * doom->screen.width) + (int)doom->player.pos.x] = 0xFF00;
-
-	//print_node(n);
+	traverseDrawOrder(doom, doom->bsp);
 }
