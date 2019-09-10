3f192c6b0fe4dc8b3fe8c69e4185cb1e2fc72e32
loucass003 <loucass003@gmail.com>
2019-09-10 17:50:57 +0200

    Add directional audio

--- srcs/audio.c
+++ srcs/audio.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   audio.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lloncham <lloncham@student.42.fr>          +#+  +:+       +#+        */
+/*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 13:41:15 by lloncham          #+#    #+#             */
-/*   Updated: 2019/09/10 15:59:58 by lloncham         ###   ########.fr       */
+/*   Updated: 2019/09/10 17:50:43 by llelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,18 +39,32 @@ void			init_SDL_mixer(t_doom *doom)
 		printf("%s", Mix_GetError());
 	// Mix_AllocateChannels(2);
 	// Mix_Volume(1, MIX_MAX_VOLUME / 2);
-	if ((music = Mix_LoadWAV("song/song0.wav")) == NULL)
+	if ((music = Mix_LoadWAV("song/song5.wav")) == NULL)
 		printf("Merde\n");
 	// Mix_VolumeChunk(music, MIX_MAX_VOLUME / 2);
 	// Mix_PlayChannel(1, music, -1);
 	Mix_CloseAudio();
 
-	source = AL_FORMAT_MONO16;
 	alGenBuffers(1, &buffer);
-	alBufferData(buffer, AL_FORMAT_STEREO16, music->abuf, music->alen, 44100);
+	alBufferData(buffer, AL_FORMAT_MONO16, music->abuf, music->alen, 44100);
+
+	ALfloat listenerOri[] = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f };
+
+	alListener3f(AL_POSITION, 20, 0, 20);
+	// check for errors
+	alListener3f(AL_VELOCITY, 0, 0, 0);
+	// check for errors
+	alListenerfv(AL_ORIENTATION, listenerOri);
 
 	alGenSources(1, &source);
 	alSourcei(source, AL_BUFFER, buffer);
+	alSourcei(source, AL_SOURCE_RELATIVE, AL_TRUE);
+
+	alSourcef(source, AL_PITCH, 2);
+	// check for errors
+	alSourcef(source, AL_GAIN, 1);
+	alSource3f(source, AL_POSITION, 1, 0, 0);
+	alSourcei(source, AL_LOOPING, AL_FALSE);
 	alSourcePlay(source);
 }
 
