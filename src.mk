3df7b6089955e345014fa66d30b541a969bbffc9
loucass003 <loucass003@gmail.com>
2019-10-19 23:41:14 +0200

    Update src.mk

--- src.mk
+++ src.mk
@@ -38,7 +38,6 @@ SRC+=components/component.c
 SRC+=components/c_button.c
 SRC+=components/menu/display_menu.c
 SRC+=components/menu/c_menu.c
-SRC+=components/c_scrollbox.c
 SRC+=components/c_progress.c
 SRC+=editor/editor_in_out.c
 SRC+=editor/editor_visual.c
