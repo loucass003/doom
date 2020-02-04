976958c459262e2a4b739bfe9341458ba1ed034b
Lisa LONCHAMP <lloncham@student.42.fr>
2020-02-04 17:33:02 +0100

    un seul boss possible

--- srcs/gui/editor_settings/object/g_es_obj_entity.c
+++ srcs/gui/editor_settings/object/g_es_obj_entity.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   g_es_obj_entity.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: louali <louali@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lloncham <lloncham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 22:55:54 by llelievr          #+#    #+#             */
-/*   Updated: 2020/01/22 11:50:37 by louali           ###   ########.fr       */
+/*   Updated: 2020/02/04 17:31:27 by lloncham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,21 @@ static t_bool			action_performed(t_component *cmp, t_doom *doom)
 	return (TRUE);
 }
 
+static t_bool	is_boss_in_map(t_doom *doom)
+{
+	int				i;
+	t_object		o;
+
+	i = -1;
+	while (++i < doom->editor.objects->len)
+	{
+		o = doom->editor.objects->values[i];
+		if (o.type == OBJECT_ENTITY && o.of.entity == ENTITY_BOSS)
+			return (TRUE);
+	}
+	return (FALSE);
+}
+
 void			g_es_obj_entity_enter(t_gui *self, t_doom *doom)
 {
 	int			x = S_WIDTH - 335;
@@ -38,8 +53,9 @@ void			g_es_obj_entity_enter(t_gui *self, t_doom *doom)
 	((t_select *)self->components->values[0])->items = create_select_items_array(1);
 	self->components->values[0]->perform_action = action_performed;
 	append_select_items_array(&((t_select *)self->components->values[0])->items, (t_select_item){ .name = "ENEMY", .value = ENTITY_ENEMY });
-	append_select_items_array(&((t_select *)self->components->values[0])->items, (t_select_item){ .name = "BOSS", .value = ENTITY_BOSS });
 	t_object	*object = &doom->editor.objects->values[doom->editor.current_object];
+	if (!is_boss_in_map(doom) || object->of.entity == ENTITY_BOSS)
+		append_select_items_array(&((t_select *)self->components->values[0])->items, (t_select_item){ .name = "BOSS", .value = ENTITY_BOSS });
 	((t_select *)self->components->values[0])->selected_item = select_items_indexof(((t_select *)self->components->values[0])->items, object->of.entity);
 }
 
