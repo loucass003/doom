08a45ac5f05a629e53d28cff2c4a3c7a71838cc2
Lisa LONCHAMP <lloncham@student.42.fr>
2020-02-08 16:11:26 +0100

    story

--- srcs/gui/g_story.c
+++ srcs/gui/g_story.c
@@ -6,7 +6,7 @@
 /*   By: lloncham <lloncham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 16:08:54 by lloncham          #+#    #+#             */
-/*   Updated: 2020/02/08 15:53:05 by lloncham         ###   ########.fr       */
+/*   Updated: 2020/02/08 16:03:34 by lloncham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,12 +42,15 @@ void			g_story_on_event(t_gui *self, SDL_Event *event, t_doom *doom)
 void			g_story_render(t_gui *self, t_doom *doom)
 {
     const char      str[5][100] = {"BONJOUR SOLDAT\0", "AUJOURD'HUI NOUS AVONS UNE MISSION POUR TOI...\0", "TUER HITLER\0", "TU ES LE SEUL A POUVOIR LE FAIRE\0", "D'ABORD TROUVE DES ITEMS\0"};
-    const char      str1[4][100] = {"SUPER TU AS TROUVER TON PREMIER ITEM\0", "TROUVE UNE ARME QUI TE PLAIT ET POURSUIS TA MISSION\0"};
+    const char      str1[4][100] = {"SUPER! TON PREMIER ITEM\0", "TROUVE UNE ARME QUI TE PLAIT ET POURSUIS TA MISSION\0"};
     const char      str2[4][100] = {"OUF...TU L'AS ECHAPPE BELLE\0"};
-    const char      str3[4][100] = {"ATTENTION!!!\0", "CE SOLDAT N'EST PAS AVEX NOUS...TIRE LUI DESSUS OU FUIS!!\0"};
+    const char      str3[4][100] = {"ATTENTION!!!\0", "CE SOLDAT N'EST PAS AVEC NOUS...TIRE LUI DESSUS OU FUIS!!\0"};
     SDL_Surface		*text;
     
-    if (doom->story.begin_story < 5 || (doom->story.first_item == TRUE && doom->story.item_story < 2) || (doom->story.first_shoot == TRUE && doom->story.shoot_story < 1) || (doom->story.first_enemy == TRUE && doom->story.enemy_story < 2))
+    if (doom->story.begin_story < 5
+        || (doom->story.first_item == TRUE && doom->story.item_story < 2)
+        || (doom->story.first_shoot == TRUE && doom->story.shoot_story < 1)
+        || (doom->story.first_enemy == TRUE && doom->story.enemy_story < 2))
     {
         fill_rect(&doom->screen, (SDL_Rect)
             { 0, S_HEIGHT - 150, S_WIDTH, 150 }, 0xFF303030);
