6c69261e7a27aee3e47f3e27a9ee02f67a6d6483
Lisa LONCHAMP <lloncham@student.42.fr>
2020-02-16 17:18:24 +0100

    norm srcs/arrays/*

--- srcs/arrays/render_datas.c
+++ srcs/arrays/render_datas.c
@@ -3,20 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   render_datas.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llelievr <llelievr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lloncham <lloncham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 14:44:37 by llelievr          #+#    #+#             */
-/*   Updated: 2020/01/27 15:04:54 by llelievr         ###   ########.fr       */
+/*   Updated: 2020/02/16 17:16:45 by lloncham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
 
-t_render_datas	*create_render_data_array(int capacity)
+t_render_datas		*create_render_data_array(int capacity)
 {
-	const size_t	size = sizeof(t_render_datas) + (capacity * sizeof(t_render_data));
-	t_render_datas			*arr;
+	t_render_datas	*arr;
+	const size_t	size;
 
+	size = sizeof(t_render_datas) + (capacity * sizeof(t_render_data));
 	if (capacity <= 0)
 		return (NULL);
 	if (!(arr = (t_render_datas*)malloc(size)))
@@ -26,14 +27,16 @@ t_render_datas	*create_render_data_array(int capacity)
 	return (arr);
 }
 
-t_render_datas	*append_render_data_array(t_render_datas **arr, t_render_data v)
+t_render_datas		*append_render_data_array(t_render_datas **arr,
+	t_render_data v)
 {
-	t_render_datas			*new;
+	t_render_datas	*new;
 	size_t			old_size;
 
 	if ((*arr)->len == (*arr)->capacity)
 	{
-		old_size = sizeof(t_render_datas) + ((*arr)->capacity * sizeof(t_render_data));
+		old_size = sizeof(t_render_datas) + ((*arr)->capacity
+			* sizeof(t_render_data));
 		(*arr)->capacity *= 2;
 		if (!(new = create_render_data_array((*arr)->capacity)))
 			return (NULL);
@@ -45,7 +48,8 @@ t_render_datas	*append_render_data_array(t_render_datas **arr, t_render_data v)
 	return (*arr);
 }
 
-t_render_datas	*splice_render_data_array(t_render_datas *arr, int index, int n)
+t_render_datas		*splice_render_data_array(t_render_datas *arr,
+	int index, int n)
 {
 	if (index < 0)
 		index = index + arr->len - 1;
@@ -57,7 +61,8 @@ t_render_datas	*splice_render_data_array(t_render_datas *arr, int index, int n)
 	return (arr);
 }
 
-int		render_datas_indexof(t_render_datas *arr, t_render_data *elem)
+int					render_datas_indexof(t_render_datas *arr,
+	t_render_data *elem)
 {
 	int	i;
 
